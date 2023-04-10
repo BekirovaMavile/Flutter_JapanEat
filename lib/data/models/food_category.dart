d2d805a858546ca76f763fad2cbade7b01434071
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-04-10 09:15:21 +0300

    Классы данных

---
+++ lib/data/models/food_category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_japan_eat/data/models/food.dart';
+
+// import '_models.dart';
+
+class FoodCategory {
+  final FoodType type;
+  bool isSelected;
+
+  FoodCategory(this.type, this.isSelected);
+}
