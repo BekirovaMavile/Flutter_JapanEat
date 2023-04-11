a2b1fbcb17775d3096edd002af980882127da22b
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2023-04-11 18:14:55 +0300

    ...

--- lib/ui/screens/food_list_screen.dart
+++ lib/ui/screens/food_list_screen.dart
@@ -1,4 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart' hide Badge;
+import 'package:flutter_japan_eat/ui_kit/_ui_kit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
 class FoodList extends StatefulWidget {
   const FoodList({super.key});
 
@@ -8,5 +13,38 @@ class FoodList extends StatefulWidget {
 
 class FoodListState extends State<FoodList> {
   @override
-  Widget build(BuildContext context) => Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      leading: IconButton(
+        icon: const FaIcon(FontAwesomeIcons.dice),
+        onPressed: () {},
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
+          Text("Location")
+        ],
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Badge(
+            badgeStyle: const BadgeStyle(
+                badgeColor: LightThemeColor.accent
+            ),
+            badgeContent: const Text(
+              "2",
+              style: TextStyle(color: Colors.white),
+            ),
+            position: BadgePosition.topStart(
+                start: -3
+            ),
+            child: const Icon(Icons.notifications_none,
+                size: 30),
+          ),
+        )
+      ],
+    ),
+  );
 }
