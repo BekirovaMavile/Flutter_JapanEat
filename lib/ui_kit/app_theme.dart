6e99e18292bc262e6ea6fa5b46d5864d9731de08
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-04-13 20:37:59 +0300

    Верстка поиска

--- lib/ui_kit/app_theme.dart
+++ lib/ui_kit/app_theme.dart
@@ -21,7 +21,22 @@ class AppTheme {
       headlineSmall: AppTextStyle.h5StyleLight,
       bodyLarge: AppTextStyle.bodyTextLight,
       titleMedium: AppTextStyle.subtitleLight,
-    )
+    ),
+    hintColor: Colors.black45,
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(borderSide: BorderSide.none),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      filled: true,
+      contentPadding: EdgeInsets.all(20),
+      fillColor: Colors.white,
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
@@ -43,6 +58,19 @@ class AppTheme {
       bodyLarge: AppTextStyle.bodyTextLight.copyWith(color: Colors.white),
       titleMedium: AppTextStyle.subtitleLight.copyWith(color: Colors.white60),
     ),
-
+    hintColor: Colors.white60,
+    inputDecorationTheme: const InputDecorationTheme(
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(25)),
+        borderSide: BorderSide(color: Colors.transparent),
+      ),
+      filled: true,
+      contentPadding: EdgeInsets.all(20),
+      fillColor: DarkThemeColor.primaryLight,
+    ),
   );
 }
