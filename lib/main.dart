c20a9ded06123dce9b8e509187f57ed0cf87eb46
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2023-04-13 14:34:51 +0300

    Верстка AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Japan Eat',
-      // theme: AppTheme.darkTheme,
-      home: FoodList(),
+      theme: AppTheme.darkTheme,
+      home: const FoodList(),
     );
   }
 }
--- lib/ui/screens/food_list_screen.dart
+++ lib/ui/screens/food_list_screen.dart
@@ -14,37 +14,43 @@ class FoodList extends StatefulWidget {
 class FoodListState extends State<FoodList> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      leading: IconButton(
-        icon: const FaIcon(FontAwesomeIcons.dice),
-        onPressed: () {},
-      ),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
-          Text("Location")
-        ],
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: Badge(
-            badgeStyle: const BadgeStyle(
-                badgeColor: LightThemeColor.accent
-            ),
-            badgeContent: const Text(
-              "2",
-              style: TextStyle(color: Colors.white),
-            ),
-            position: BadgePosition.topStart(
-                start: -3
-            ),
-            child: const Icon(Icons.notifications_none,
-                size: 30),
-          ),
+    appBar: _appBar(context),
+  );
+}
+
+PreferredSizeWidget _appBar(BuildContext context){
+  return AppBar(
+    leading: IconButton(
+      icon: const FaIcon(FontAwesomeIcons.dice),
+      onPressed: () {},
+    ),
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.location_on_outlined, color: LightThemeColor.accent),
+        Text("Location",
+          style: Theme.of(context).textTheme.bodyLarge,
         )
       ],
     ),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: Badge(
+          badgeStyle: const BadgeStyle(
+              badgeColor: LightThemeColor.accent
+          ),
+          badgeContent: const Text(
+            "2",
+            style: TextStyle(color: Colors.white),
+          ),
+          position: BadgePosition.topStart(
+              start: -3
+          ),
+          child: const Icon(Icons.notifications_none,
+              size: 30),
+        ),
+      )
+    ],
   );
 }
