b964e3b82de75dbcc682fd52db30653a64aea0c9
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-05-07 14:56:41 +0300

    Список еды в корзине

--- lib/states/food_state.dart
+++ lib/states/food_state.dart
@@ -47,6 +47,11 @@ class FoodState {
     if (!cartIds.contains(id)) cartIds.add(id);
     foods[id]?.quantity = amount;
   }
+  String calculatePricePerEachItem(Food food) {
+    double price = 0;
+    price = food.quantity * food.price;
+    return price.toString();
+  }
 
 
 
