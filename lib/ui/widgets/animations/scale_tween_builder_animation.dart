bcd3477d049aa5a8b7d2fc48a8c083417280d6a9
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2023-06-30 13:53:20 +0300

    Примеры неявной анимации

--- lib/ui/widgets/animations/scale_tween_builder_animation.dart
+++ lib/ui/widgets/animations/scale_tween_builder_animation.dart
@@ -7,15 +7,48 @@ class ScaleTweenAnimationsBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder(
+      // duration: const Duration(seconds: 2),
+      // tween: Tween<double>(begin: 0.1, end: 1),
+      // builder: (_, double scale, __) {
+      //   return Transform.scale(
+      //     scale: scale,
+      //     child: child,
+      //   );
+      // },
+      // curve: Curves.bounceOut,
+
+      // duration: const Duration(seconds: 2),
+      // tween: Tween<double>(begin: 0, end: 2 * 3.14),
+      // builder: (_, double angle, __) {
+      //   return Transform.rotate(
+      //     angle: angle,
+      //     child: child,
+      //   );
+      // },
+
+      // duration: const Duration(seconds: 2),
+      // tween: Tween<double>(begin: 0, end: 1),
+      // builder: (_, double opacity, __) {
+      //   return Opacity(
+      //     opacity: opacity,
+      //     child: child,
+      //   );
+      // },
+      // curve: Curves.easeOut,
+
       duration: const Duration(seconds: 2),
-      tween: Tween<double>(begin: 0.1, end: 1),
-      builder: (_, double scale, __) {
-        return Transform.scale(
-          scale: scale,
+      tween: Tween<double>(begin: 10, end: 300),
+      builder: (_, double size, __) {
+        return SizedBox(
+          width: size,
+          height: size,
           child: child,
         );
       },
       curve: Curves.bounceOut,
+
+
+
     );
   }
 }
