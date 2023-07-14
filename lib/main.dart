f6a537d825eafe331d76877173abb827151ef4bc
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2023-07-14 12:52:42 +0300

    Simple animations package

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_japan_eat/states/food_state.dart';
 import 'package:simple_animations/simple_animations.dart';
+
+import 'data/models/food.dart';
 
 void main() =>
     runApp(const MaterialApp(home: Scaffold(body: Center(child: Page()))));
 
-class Page extends StatefulWidget {
+class Page extends StatelessWidget {
   const Page({Key? key}) : super(key: key);
+  Food get food => FoodState().foodById(foodId);
 
-  @override
-  _PageState createState() => _PageState();
-}
+  int get foodId => FoodState().selectedFood;
 
-class _PageState extends State<Page> {
-  Control control = Control.play; // state variable
+
 
   @override
   Widget build(BuildContext context) {
-    return CustomAnimationBuilder<double>(
-      duration: const Duration(seconds: 1),
-      control: control, // bind state variable to parameter
-      tween: Tween(begin: -100.0, end: 100.0),
+    return PlayAnimationBuilder<double>(
+      tween: Tween(begin: 100.0, end: 200.0),
+      duration: const Duration(seconds: 2),
+      delay: const Duration(seconds: 1),
+      curve: Curves.easeOut,
       builder: (context, value, child) {
-        return Transform.translate(
-          // animation that moves childs from left to right
-          offset: Offset(value, 0),
-          child: child,
+        return ElevatedButton(
+          onPressed: (){},
+          // children:[
+          // const Text("Checkout"),
+        child: Container(
+          width: value,
+          height: 50.0,
+        // child: const Text("Checkout"),
+        //   color: Colors.orange,
+        ),
+
         );
       },
-      child: MaterialButton(
-        // there is a button
-        color: Colors.yellow,
-        onPressed:
-        toggleDirection, // clicking button changes animation direction
-        child: const Text('Swap'),
-      ),
     );
   }
-
-  void toggleDirection() {
-    // toggle between control instructions
-    setState(() {
-      control = (control == Control.play) ? Control.playReverse : Control.play;
-    });
-  }
+    // return PlayAnimationBuilder<double>(
+    //   tween: Tween(begin: 0.0, end: 200.0),
+    //   duration: const Duration(seconds: 2),
+    //   curve: Curves.easeOut,
+    //   builder: (context, value, child) {
+    //     return Image.asset(food.image, width: value, height: value);
+    //     // return Container(
+    //     //   width: value,
+    //     //   height: value,
+    //     //   color: Colors.pink,
+    //     // );
+    //   },
+    // );
 }
